dca67dace46823212a3081a0a9f9166b9e475e21
DukesCounter <dukescounter@sharklasers.com>
2016-10-30 15:53:17 -0400

    Add files via upload

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 
 // configure
 $from = 'info@dukesgrocery.com'; 
-$sendTo = 'dukescounter@gmail.com';
+$sendTo = 'jacob.wood@gmail.com';
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in email
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
